a7b77735a764e7efd496fc77a2db9381c2ff44e0
JOTAGGE <108813623+JOTAGGE@users.noreply.github.com>
2023-09-22 23:41:53 +0000

    frst commit

---
+++ Disciplinas.java
@@ -0,0 +1,12 @@
+import java.util.Arraylist;
+
+public class Disciplina{
+    private String nome;
+    private String professor;
+
+    public Disciplina(String nome){
+        this.nome = noem;
+        this.professor = null;
+    }
+    //getters e setters da classe
+}
---
+++ professor.java
@@ -0,0 +1,17 @@
+public class Professor{
+    private String nome;
+    private Arraylist<Disciplina> disciplinas;
+
+    public Professor(String nome){
+        this.nome = nome;
+        this.disciplinas = null;
+    }
+
+    //getters e setters da classe
+    public void add Disciplina(Disciplina diciplina){
+        if(disciplinas == null){
+            disciplinas = new Arrayulist<>();
+
+        }
+    }
+}
